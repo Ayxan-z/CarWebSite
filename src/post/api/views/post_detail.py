f8a33b5ae3d1515208e060b6bbb7712b1a80e30f
ayxan <ayxan.shahsuvarov1@gmail.com>
2022-09-08 00:43:54 +0400

    RetrieveAPIView -> APIView

--- src/post/api/views/post_detail.py
+++ src/post/api/views/post_detail.py
@@ -1,8 +1,6 @@
-from rest_framework.generics import RetrieveAPIView
 from post.models import PostModel
 from post.api.serializers import PostDetailSerializer
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
 
