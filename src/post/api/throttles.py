28d67f83c15d1a3dbc379f397c17720341677a9b
ayxan <ayxan.shahsuvarov1@gmail.com>
2022-10-10 12:23:02 +0400

    bug fixed DetailViewThrottle

--- src/post/api/throttles.py
+++ src/post/api/throttles.py
@@ -3,9 +3,11 @@ from rest_framework.throttling import SimpleRateThrottle
 
 class DetailViewThrottle(SimpleRateThrottle):
     scope = 'detailviewthrottle'
+    cache_format = 'throttle_%(scope)s_%(ident)s_%(detail)s'
 
     def get_cache_key(self, request, view):
         return self.cache_format % {
             'scope': self.scope,
-            'ident': self.get_ident(request)
+            'ident': self.get_ident(request),
+            'detail': view.kwargs.get('pk')
         }
